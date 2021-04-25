41def9bae54b96485510a935c3cda0ed65603939
german-zarate <germanzaratevalbuena@gmail.com>
2021-04-25 23:08:17 +0300

    Improve country list generation logic

--- countries.py
+++ countries.py
@@ -6,7 +6,7 @@ def create_list():
     result = db.session.execute(sql)
     db.session.commit()
 
-    if result.fetchone()[0] > 1:
+    if result.fetchone()[0] > 0:
         return False
 
     sql = "INSERT INTO continents (name) VALUES " \
@@ -18,18 +18,18 @@ def create_list():
         reader = csv.reader(file, delimiter=',')
         for row in reader:
             name = row[1]
-            continent_id = ""
+            continent_id = 0
             if row[0] == "AF":
                 continent_id = 1
-            if row[0] == "AS":
+            elif row[0] == "AS":
                 continent_id = 2
-            if row[0] == "EU":
+            elif row[0] == "EU":
                 continent_id = 3
-            if row[0] == "NA":
+            elif row[0] == "NA":
                 continent_id = 4
-            if row[0] == "SA":
+            elif row[0] == "SA":
                 continent_id = 5
-            if row[0] == "OC":
+            elif row[0] == "OC":
                 continent_id = 6
             sql = "INSERT INTO countries (name, continent_id) VALUES (:name, :continent_id)"
             db.session.execute(sql, {"name":name, "continent_id":continent_id})
