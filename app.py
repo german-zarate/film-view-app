009e6fea2f5fc4c2f749484572fae819a6731295
german-zarate <germanzaratevalbuena@gmail.com>
2021-04-28 11:25:55 +0300

    Add error handler for 404

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 from os import getenv
 
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
+
+@app.errorhandler(404)
+def page_not_found(error):
+   return render_template("error.html", message="404 - Page not found")
 
 import routes
