563c70414ab20f52da4eb90cd8dd6a9ecd4169e5
german-zarate <germanzaratevalbuena@gmail.com>
2021-04-28 22:37:23 +0300

    Add timestamp to review and display it on the film page

--- reviews.py
+++ reviews.py
@@ -2,13 +2,15 @@ from db import db
 import users
 
 def get_list(film_id):
-    sql = "SELECT u.username, r.content, r.grade FROM reviews AS r, users AS u " \
+    sql = "SELECT u.username, r.content, r.grade, TO_CHAR(r.submitted, 'DD Month - HH24:MI') " \
+          "FROM reviews AS r, users AS u " \
           "WHERE r.film_id=:film_id AND u.id=r.user_id"
     result = db.session.execute(sql, {"film_id":film_id})
     return result.fetchall()
 
 def send(user_id, film_id, content, grade):
-    sql = "INSERT INTO reviews (user_id, film_id, content, grade) VALUES (:user_id, :film_id, :content, :grade)"
+    sql = "INSERT INTO reviews (user_id, film_id, content, grade, submitted) " \
+          "VALUES (:user_id, :film_id, :content, :grade, NOW())"
     db.session.execute(sql, {"user_id":user_id, "film_id":film_id, "content":content, "grade":grade})
     db.session.commit()
     return True
