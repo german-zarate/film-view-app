7875f849333e3a2970914f0279c907f0610e5585
german-zarate <germanzaratevalbuena@gmail.com>
2021-05-06 22:40:39 +0300

    Add error handlers for codes 400 and 500

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ from os import getenv
 app = Flask(__name__)
 app.secret_key = getenv("SECRET_KEY")
 
+@app.errorhandler(400)
+def bad_request(error):
+   return render_template("error.html", message="400 - Bad request")
+
 @app.errorhandler(403)
 def forbidden(error):
    return render_template("error.html", message="403 - Forbidden")
@@ -11,5 +15,9 @@ def forbidden(error):
 @app.errorhandler(404)
 def page_not_found(error):
    return render_template("error.html", message="404 - Page not found")
+
+@app.errorhandler(500)
+def internal_server_error(error):
+   return render_template("error.html", message="500 - Internal server error")
 
 import routes
