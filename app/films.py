b61ded22015e517a002ecb7b0b438cfb9a90631a
german-zarate <germanzaratevalbuena@gmail.com>
2021-06-21 00:14:12 +0300

    Sort functions alphabetically

--- app/films.py
+++ app/films.py
@@ -6,16 +6,56 @@ def count():
     result = db.session.execute(sql)
     return result.fetchone()[0]
 
+def delete(id):
+    sql = "UPDATE films SET visible=0 WHERE id=:id"
+    db.session.execute(sql, {"id":id})
+    db.session.commit()
+    return True
+
 def exists(id):
     sql = "SELECT COUNT(1) FROM films WHERE id=:id AND visible=1"
     result = db.session.execute(sql, {"id":id})
     if result.fetchone()[0] == 0:
         abort(404)
 
-def visible(id):
-    sql = "SELECT visible FROM films WHERE id=:id"
+def get_all():
+    sql = "SELECT f.id, f.visible, f.name, f.description, f.year " \
+          "FROM films AS f " \
+          "ORDER BY f.name"
+    result = db.session.execute(sql)
+    return result.fetchall()
+
+def get_details(id):
+    sql = "SELECT f.id, f.name, f.description, f.year, c.name, l.name, " \
+          "g.name, d.name, s.name FROM films AS f, countries AS c, " \
+          "languages AS l, genres AS g, directors AS d, screenwriters AS s " \
+          "WHERE f.id=:id AND c.id=f.country_id AND l.id=f.language_id " \
+          "AND g.id=f.genre_id AND d.id=f.director_id " \
+          "AND s.id=f.screenwriter_id"
+    result = db.session.execute(sql, {"id":id})
+    return result.fetchall()
+
+def get_name(id):
+    sql = "SELECT name FROM films WHERE id=:id"
     result = db.session.execute(sql, {"id":id})
     return result.fetchone()[0]
+
+def get_search_results(query):
+    sql = "(SELECT f.id AS s_id, f.visible, f.name AS s_name, " \
+          "f.description, f.year AS s_year, c.name, c.code, " \
+          "ROUND(AVG(r.grade),1) AS s_avg FROM films AS f, countries AS c, " \
+          "reviews AS r, users AS u WHERE f.id=r.film_id AND " \
+          "c.id=f.country_id AND visible=1 AND u.id=r.user_id AND " \
+          "u.banned=0 AND f.name LIKE :query GROUP BY f.id, c.name, c.code) " \
+          "UNION (SELECT f.id, f. visible, f.name, f.description, f.year, " \
+          "c.name, c.code, 0.0 FROM films AS f, countries AS c, " \
+          "reviews AS r, users AS u WHERE c.id=f.country_id AND visible=1 " \
+          "AND f.name LIKE :query AND NOT EXISTS (SELECT * FROM " \
+          "reviews AS r WHERE f.id=r.film_id) AND u.id=r.user_id " \
+          "AND u.banned=0) ORDER BY s_name"
+    result = db.session.execute(sql, {"query":"%"+query+"%",
+                                      "queryGROUP":"%"+query+"%"})
+    return result.fetchall()
 
 def get_visible(sort_by):
     sql = "(SELECT f.id AS s_id, f.visible, f.name AS s_name, " \
@@ -42,44 +82,11 @@ def get_visible(sort_by):
     result = db.session.execute(sql)
     return result.fetchall()
 
-def get_search_results(query):
-    sql = "(SELECT f.id AS s_id, f.visible, f.name AS s_name, " \
-          "f.description, f.year AS s_year, c.name, c.code, " \
-          "ROUND(AVG(r.grade),1) AS s_avg FROM films AS f, countries AS c, " \
-          "reviews AS r, users AS u WHERE f.id=r.film_id AND " \
-          "c.id=f.country_id AND visible=1 AND u.id=r.user_id AND " \
-          "u.banned=0 AND f.name LIKE :query GROUP BY f.id, c.name, c.code) " \
-          "UNION (SELECT f.id, f. visible, f.name, f.description, f.year, " \
-          "c.name, c.code, 0.0 FROM films AS f, countries AS c, " \
-          "reviews AS r, users AS u WHERE c.id=f.country_id AND visible=1 " \
-          "AND f.name LIKE :query AND NOT EXISTS (SELECT * FROM " \
-          "reviews AS r WHERE f.id=r.film_id) AND u.id=r.user_id " \
-          "AND u.banned=0) ORDER BY s_name"
-    result = db.session.execute(sql, {"query":"%"+query+"%",
-                                      "queryGROUP":"%"+query+"%"})
-    return result.fetchall()
-
-def get_all():
-    sql = "SELECT f.id, f.visible, f.name, f.description, f.year " \
-          "FROM films AS f " \
-          "ORDER BY f.name"
-    result = db.session.execute(sql)
-    return result.fetchall()
-
-def get_details(id):
-    sql = "SELECT f.id, f.name, f.description, f.year, c.name, l.name, " \
-          "g.name, d.name, s.name FROM films AS f, countries AS c, " \
-          "languages AS l, genres AS g, directors AS d, screenwriters AS s " \
-          "WHERE f.id=:id AND c.id=f.country_id AND l.id=f.language_id " \
-          "AND g.id=f.genre_id AND d.id=f.director_id " \
-          "AND s.id=f.screenwriter_id"
-    result = db.session.execute(sql, {"id":id})
-    return result.fetchall()
-
-def get_name(id):
-    sql = "SELECT name FROM films WHERE id=:id"
-    result = db.session.execute(sql, {"id":id})
-    return result.fetchone()[0]
+def restore(id):
+    sql = "UPDATE films SET visible=1 WHERE id=:id"
+    db.session.execute(sql, {"id":id})
+    db.session.commit()
+    return True
 
 def send(name, description, year, country_id, language_id,
          genre_id, director_id, screenwriter_id):
@@ -97,14 +104,7 @@ def send(name, description, year, country_id, language_id,
     db.session.commit()
     return True
 
-def delete(id):
-    sql = "UPDATE films SET visible=0 WHERE id=:id"
-    db.session.execute(sql, {"id":id})
-    db.session.commit()
-    return True
-
-def restore(id):
-    sql = "UPDATE films SET visible=1 WHERE id=:id"
-    db.session.execute(sql, {"id":id})
-    db.session.commit()
-    return True
+def visible(id):
+    sql = "SELECT visible FROM films WHERE id=:id"
+    result = db.session.execute(sql, {"id":id})
+    return result.fetchone()[0]
