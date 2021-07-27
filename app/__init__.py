f202977f5361493b665264e5a5c2bee06d4e2f05
german-zarate <germanzaratevalbuena@gmail.com>
2021-07-27 23:38:18 +0300

    Fix indentation

--- app/__init__.py
+++ app/__init__.py
@@ -8,22 +8,22 @@ app.secret_key = getenv("SECRET_KEY")
 
 @app.errorhandler(400)
 def bad_request(error):
-   return render_template("error.html", message="400 - Bad request")
+    return render_template("error.html", message="400 - Bad request")
 
 
 @app.errorhandler(403)
 def forbidden(error):
-   return render_template("error.html", message="403 - Forbidden")
+    return render_template("error.html", message="403 - Forbidden")
 
 
 @app.errorhandler(404)
 def page_not_found(error):
-   return render_template("error.html", message="404 - Page not found")
+    return render_template("error.html", message="404 - Page not found")
 
 
 @app.errorhandler(500)
 def internal_server_error(error):
-   return render_template("error.html", message="500 - Internal server error")
+    return render_template("error.html", message="500 - Internal server error")
 
 
 from app import routes
